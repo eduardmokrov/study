94d309d136d56d191c023b724cddb5cb97192f57
eduardmokrov <mokrov.eduard@gmail.com>
2016-08-23 10:29:39 +0400

    Add files via upload

---
+++ wp-content/plugins/itstudy/templates/course.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * template for siplaying courses
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+get_header();
+echo 'course_page';
+get_footer();
+
